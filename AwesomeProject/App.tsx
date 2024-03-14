7535efb517f810fe0e83d6c99f3cf13e8cdc817a
Utkarsh <utkarshsinghal369@gmail.com>
2024-03-14 18:36:41 +0530

    not good ui design need

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {View, StatusBar, Text} from 'react-native';
+import {View, StatusBar, Text, StyleSheet} from 'react-native';
 import {NavigationContainer, DrawerActions} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
@@ -9,46 +9,29 @@ import Icon from 'react-native-vector-icons/AntDesign';
 
 import DrawerContent from './src/component/DrawerContent';
 import Home from './src/screen/Home';
-import User from './src/screen/User';
-import Profile from './src/screen/Profile';
 import BrowserCar from './src/screen/BrowserCar';
 import Favourites from './src/screen/Favourites';
 import RentalHistory from './src/screen/RentalHistory';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+
 interface CustomHeaderProps {
   title: string;
   navigation: any;
-  tc?: boolean; // Optional boolean prop for text centering
 }
+
 const CustomHeader: React.FC<CustomHeaderProps> = ({title, navigation}) => {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#0163d2',
-        paddingHorizontal: 10,
-        paddingVertical: 15,
-      }}>
+    <View style={styles.headerContainer}>
       <Icon
         name="menuunfold"
-        size={32}
+        size={28}
         color={'#fff'}
         onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
       />
       <StatusBar backgroundColor="#0163d2" barStyle="light-content" />
-      <Text
-        style={{
-          color: '#fff',
-          fontSize: 20,
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}>
-        {title}
-      </Text>
+      <Text style={styles.titleText}>{title}</Text>
     </View>
   );
 };
@@ -72,13 +55,11 @@ const DrawerNav = () => {
         header: ({navigation, route}) => (
           <CustomHeader title={route.name} navigation={navigation} />
         ),
-        drawerIcon: () => (
-          <MaterialIcons name={'house'} size={24} color={'#000'} />
+        drawerIcon: ({focused, color, size}) => (
+          <MaterialIcons name={'house'} size={size} color={color} />
         ),
       }}>
       <Drawer.Screen name="Home" component={StackNav} />
-      <Drawer.Screen name="User" component={User} />
-      <Drawer.Screen name="Profile" component={Profile} />
       <Drawer.Screen name="BrowserCar" component={BrowserCar} />
       <Drawer.Screen name="Favourite" component={Favourites} />
       <Drawer.Screen name="RentalHistory" component={RentalHistory} />
@@ -95,3 +76,19 @@ function App() {
 }
 
 export default App;
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0163d2',
+    paddingHorizontal: 10,
+    paddingVertical: 20, // Increased header height
+  },
+  titleText: {
+    color: '#fff',
+    fontSize: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+});
--- AwesomeProject/src/screen/Home.tsx
+++ AwesomeProject/src/screen/Home.tsx
@@ -1,13 +1,85 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useLayoutEffect, useState} from 'react';
+import {Image, View, StyleSheet, Dimensions} from 'react-native';
+import axios from 'axios';
+import {FlatList} from 'react-native-gesture-handler';
+import {Text} from 'react-native-paper';
+const screenWidth = Dimensions.get('window').width * 0.9;
 
-const Home = () => {
+const CarItem = ({item}) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.surface}>
+      <View style={styles.carItem}>
+        <Image source={{uri: item.image}} style={styles.carImage} />
+        <View style={styles.carInfo}>
+          <Text style={styles.carMakeModel}>
+            {item.make} {item.model}
+          </Text>
+          <Text style={{textAlign: 'center'}}>{item.color}</Text>
+          <Text style={styles.carPrice}>${item.price}</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      let res = await axios.get(`https://ruby-giant-crocodile.cyclic.app/car`);
+      setData(res.data);
+    } catch (err) {
+      console.log('err');
+    }
+  };
+
+  useLayoutEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={item => item._id}
+      renderItem={({item}) => <CarItem item={item} />}
+      horizontal
+    />
+  );
+};
+
 export default Home;
+
+const styles = StyleSheet.create({
+  carInfo: {
+    marginLeft: 10,
+    textAlign: 'center',
+  },
+  carMakeModel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  carPrice: {
+    fontSize: 16,
+    color: 'green',
+    textAlign: 'center',
+  },
+  carItem: {
+    flex: 1,
+    marginRight: 20,
+  },
+  carImage: {
+    width: screenWidth,
+    height: 300,
+    borderRadius: 30,
+    resizeMode: 'cover',
+  },
+  hoveredImage: {
+    transform: [{scale: 1.1}],
+  },
+  surface: {
+    marginTop: 20,
+  },
+});
